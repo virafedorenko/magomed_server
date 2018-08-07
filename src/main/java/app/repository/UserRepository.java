38888e21c5b0db84ddf136203821aaf55d8eece0
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-07 17:29:52 +0300

    Change hibernate confs

--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -3,7 +3,9 @@ package app.repository;
 import app.entity.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
     @Query("select s from users s where s.email <= ?")

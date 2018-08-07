f109028da35e1a82d747f53df2df617e7d0aba83
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-07 15:47:15 +0300

    Change hibernate confs

--- src/main/java/app/repository/impl/UserRepositoryImpl.java
+++ src/main/java/app/repository/impl/UserRepositoryImpl.java
@@ -2,31 +2,26 @@ package app.repository.impl;
 
 import app.entity.User;
 import app.repository.UserRepository;
-import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import org.hibernate.SessionFactory;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-    private EntityManagerFactory entityManagerFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public User findUserById(String id) {
-        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
-        return session.get(User.class, id);
+        return sessionFactory.getCurrentSession().get(User.class, id);
     }
 
     @Override
     public User findUserByEmail(String email) {
-        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
-        Query query = session.createQuery("FROM User p where email=:email");
+        Query query = sessionFactory.getCurrentSession().createQuery("FROM User p where email=:email");
         query.setParameter("email", email);
         if (query.getResultList().size() != 0) {
             return (User) query.getResultList().get(0);
@@ -36,7 +31,6 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User add(User user) {
-        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
-        return (User) session.save(user);
+        return (User) sessionFactory.getCurrentSession().save(user);
     }
 }

d74a20f5b60f2b88264a7b720adaf01d918e7cf7
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-07 15:44:27 +0300

    Change hibernate confs

--- src/main/java/app/repository/impl/UserRepositoryImpl.java
+++ src/main/java/app/repository/impl/UserRepositoryImpl.java
@@ -2,6 +2,7 @@ package app.repository.impl;
 
 import app.entity.User;
 import app.repository.UserRepository;
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,21 +10,23 @@ import org.springframework.stereotype.Repository;
 import org.hibernate.SessionFactory;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    private EntityManagerFactory entityManagerFactory;
 
     @Override
     public User findUserById(String id) {
-        return sessionFactory.getCurrentSession().get(User.class, id);
+        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
+        return session.get(User.class, id);
     }
 
     @Override
     public User findUserByEmail(String email) {
-        Query query = sessionFactory.getCurrentSession().createQuery("FROM User p where email=:email");
+        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
+        Query query = session.createQuery("FROM User p where email=:email");
         query.setParameter("email", email);
         if (query.getResultList().size() != 0) {
             return (User) query.getResultList().get(0);
@@ -33,6 +36,7 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User add(User user) {
-        return (User) sessionFactory.getCurrentSession().save(user);
+        Session session = entityManagerFactory.unwrap(SessionFactory.class).openSession();
+        return (User) session.save(user);
     }
 }

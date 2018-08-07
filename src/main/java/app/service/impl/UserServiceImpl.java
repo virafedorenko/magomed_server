8a94823cc838fd8d57da0affeb70b333af06ea3e
Anastasiia Fedorenko <Anastasiia_Fedorenko@epam.com>
2018-08-07 08:56:08 +0300

    Add annotation to UserServiceimpl

--- src/main/java/app/service/impl/UserServiceImpl.java
+++ src/main/java/app/service/impl/UserServiceImpl.java
@@ -5,7 +5,9 @@ import app.repository.UserRepository;
 import app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;

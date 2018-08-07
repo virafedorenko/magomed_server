76e1440d392dff85048e3db54aaf47eb7d4de152
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-07 15:16:20 +0300

    Change hibernate confs

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,8 +2,9 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JpaRepositoriesAutoConfiguration.class)
 public class Application  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

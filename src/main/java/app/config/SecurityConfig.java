34a0aad97b3516c64e60d5aad2489fd529ea04b0
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-14 13:30:12 +0300

    Change security for swagger

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login/**", "/register")
+                .antMatchers("/login/**", "/register","/v2/api-docs")
                 .permitAll()
                 .anyRequest()
                 .authenticated();

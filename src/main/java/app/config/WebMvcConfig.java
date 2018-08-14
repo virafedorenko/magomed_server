6032d7ecdffe15f90f982130de5c818feee01d82
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-14 18:21:05 +0300

    Change conf code for swagger ui

---
+++ src/main/java/app/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package app.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}

42a29d00c8fb9e95054b30e1bbadb093833969c6
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-15 13:30:18 +0300

    Cors setup

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import app.auth.jwt.JwtAuthenticationEntryPoint;
 import app.auth.jwt.JwtAuthenticationFilter;
 import app.auth.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.IgnoredRequestCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -55,7 +61,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-
+    @Bean
+    public IgnoredRequestCustomizer optionsIgnoredRequestsCustomizer() {
+        return configurer -> {
+            List<RequestMatcher> matchers = new ArrayList<>();
+            matchers.add(new AntPathRequestMatcher("/**", "OPTIONS"));
+            configurer.requestMatchers(new OrRequestMatcher(matchers));
+        };
+    }
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {

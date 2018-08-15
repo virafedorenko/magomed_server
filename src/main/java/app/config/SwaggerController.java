ff468d81688d3020a3df9ea755566492ded989c3
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-15 11:06:48 +0300

    Add cors just for test

---
+++ src/main/java/app/config/SwaggerController.java
@@ -0,0 +1,16 @@
+package app.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class SwaggerController {
+    @CrossOrigin(origins = "https://floating-citadel-49508.herokuapp.com")
+    @RequestMapping(method = RequestMethod.GET)
+    public String swaggerUi() {
+        return "redirect:/swagger-ui.html";
+    }
+}

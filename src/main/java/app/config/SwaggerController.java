b845990482e32505b48beced8c2b3ae9b7b8610c
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-15 14:00:06 +0300

    Cors setup

--- src/main/java/app/config/SwaggerController.java
+++ src/main/java/app/config/SwaggerController.java
@@ -1,26 +1,26 @@
 package app.config;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/")
 public class SwaggerController {
-
+/*
     @RequestMapping(method = RequestMethod.GET)
     public String swaggerUi() {
         return "redirect:/swagger-ui.html";
-    }
-    @RequestMapping(
-            value = "/**",
-            method = RequestMethod.OPTIONS
-    )
-    public ResponseEntity handle() {
-        return new ResponseEntity(HttpStatus.OK);
+    }*/
+    @RequestMapping(value= "/swagger-ui.html/**", method=RequestMethod.OPTIONS)
+    public void corsHeaders(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+        response.addHeader("Access-Control-Max-Age", "3600");
     }
 }

af92f3c67be434df37122c79aa735f2de8f95900
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-27 17:22:12 +0300

    Experiment with jpa

--- src/main/java/app/service/impl/TrackingObjectServiceImpl.java
+++ src/main/java/app/service/impl/TrackingObjectServiceImpl.java
@@ -32,6 +32,6 @@ public class TrackingObjectServiceImpl implements TrackingObjectService {
 
     @Override
     public List<TrackingObject> getByUser(String userId) {
-        return trackingObjectRepository.getByUser(userId);
+        return trackingObjectRepository.findByUser(userId);
     }
 }

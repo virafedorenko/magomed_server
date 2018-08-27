7a4478f46f514f4ce7e4c30caacedb8c718e9b50
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-27 17:36:04 +0300

    Remove unnecessary code

--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package app.repository;
 
 import app.entity.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
-    @Query("select s from User s where s.email=:email")
     User findByEmail(@Param("email") String email);
-
 }

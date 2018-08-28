422f342daab202de2f4b340d93f1686ca7a2f54b
Anastasiia_Fedorenko <nastia.federenko@gmail.com>
2018-08-28 12:58:09 +0300

    Add check for authenticated user in getObject method

--- src/main/java/app/controller/ObjectsController.java
+++ src/main/java/app/controller/ObjectsController.java
@@ -1,5 +1,6 @@
 package app.controller;
 
+import app.auth.UserPrincipal;
 import app.entity.TrackingObject;
 import app.entity.User;
 import app.exception.ResourceNotFoundException;
@@ -8,6 +9,8 @@ import app.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +29,11 @@ public class ObjectsController {
         TrackingObject object = trackingObjectService.getById(id);
         if (object == null) {
             throw new ResourceNotFoundException("Object", "id", id);
+        }
+        User user = object.getUser();
+        String authenticatedUserId = ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        if (!user.getId().equals(authenticatedUserId)) {
+            throw new AccessDeniedException("You cannot access not yours objects!");
         }
         return object;
     }
